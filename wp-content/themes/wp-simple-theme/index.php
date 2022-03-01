721c762b518f578c6a241a150d6481cebf29046c
Leandro Arturi <lea.arturi@gmail.com>
2022-03-01 13:56:40 -0300

    Loop entradas

--- wp-content/themes/wp-simple-theme/index.php
+++ wp-content/themes/wp-simple-theme/index.php
@@ -4,42 +4,28 @@
 <div class="container">
 
     <div class="row mt-4 mb-4">
-        <div class="col-12 col-sm-6 col-md-4 mb-3">
-            <div class="card">
-                <img class="card-img-top img-fluid" src="images/articulo.png" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+        <?php if (have_posts()) : while (have_posts()) : the_post();  ?>
+
+                <div class="col-12 col-sm-6 col-md-4 mb-3">
+                    <div class="card">
+                        <img class="card-img-top img-fluid" src="images/articulo.png" alt="Card image cap">
+                        <div class="card-body">
+                            <a href="<?php the_permalink(); ?>">
+                                <h5 class="card-title"><?php the_title(); ?></h5>
+                            </a>
+                            <p class="card-text"><?php the_excerpt(); ?></p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muter"><?php echo get_the_date(); ?> * <?php the_category(', '); ?> * <?php the_author(); ?></small>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-footer">
-                    <small class="text-muter">Fecha / Categorias / Autor</small>
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 mb-3">
-            <div class="card">
-                <img class="card-img-top img-fluid" src="images/articulo.png" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muter">Fecha / Categorias / Autor</small>
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 mb-3">
-            <div class="card">
-                <img class="card-img-top img-fluid" src="images/articulo.png" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muter">Fecha / Categorias / Autor</small>
-                </div>
-            </div>
-        </div>
+
+        <?php endwhile;
+        endif; ?>
+
+
     </div>
 </div>
 <!-- Fin Contenido -->
